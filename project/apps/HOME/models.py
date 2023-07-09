0f7b08eaaa8ed7c23f53a1a98eaac39b3a40ba93
Alejandra Caicedo <alejacr34@gmail.com>
2023-07-09 12:43:39 -0400

    creado index y corregido models

--- project/apps/HOME/models.py
+++ project/apps/HOME/models.py
@@ -7,20 +7,30 @@ class BodegaProveedor(models.Model):
     direccion = models.CharField(max_length=200)
     # Otros campos y relaciones
 
+    def __str__(self):
+        return self.nombre
+    
 class BodegaPrincipal(models.Model):
     # Atributos del modelo
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     # Otros campos y relaciones
 
+    def __str__(self):
+        return self.nombre
 class Cliente(models.Model):
     # Atributos del modelo
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     # Otros campos y relaciones
-
+    def __str__(self):
+        return self.nombre
 class Cilindro(models.Model):
     # Atributos del modelo
     codigo = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
     capacidad = models.IntegerField()
     # Otros campos y relaciones
+
+    def __str__(self):
+        return self.codigo
