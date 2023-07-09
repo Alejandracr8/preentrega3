a96ce7d51cfabaca8d7e5617307618dc4c902ae1
Alejandra Caicedo <alejacr34@gmail.com>
2023-07-09 11:49:04 -0400

    Creados modelos, Bod proveedor, Bod principal, Cliente, y cilindro

--- project/apps/HOME/models.py
+++ project/apps/HOME/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class BodegaProveedor(models.Model):
+    # Atributos del modelo
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    # Otros campos y relaciones
+
+class BodegaPrincipal(models.Model):
+    # Atributos del modelo
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    # Otros campos y relaciones
+
+class Cliente(models.Model):
+    # Atributos del modelo
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    # Otros campos y relaciones
+
+class Cilindro(models.Model):
+    # Atributos del modelo
+    codigo = models.CharField(max_length=50)
+    capacidad = models.IntegerField()
+    # Otros campos y relaciones
