91b2e5ddc8795161e8f5cd7d8291a00b8a75ece6
Alejandra Caicedo <alejacr34@gmail.com>
2023-07-09 15:57:57 -0400

    Actualizado el modulo models

--- project/apps/HOME/models.py
+++ project/apps/HOME/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+
+class Cilindro(models.Model):
+    # Atributos del modelo
+    codigo = models.CharField(max_length=50)
+    capacidad = models.IntegerField()
+    
+
+    def __str__(self):
+        return self.codigo
 # Create your models here.
 class BodegaProveedor(models.Model):
     # Atributos del modelo
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
-    # Otros campos y relaciones
+    codigo_cilindro = models.ForeignKey(Cilindro, on_delete=models.SET_NULL, null=True)
+    
 
     def __str__(self):
         return self.nombre
@@ -14,7 +24,8 @@ class BodegaPrincipal(models.Model):
     # Atributos del modelo
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
-    # Otros campos y relaciones
+    codigo_cilindro = models.ForeignKey(Cilindro, on_delete=models.SET_NULL, null=True)
+    
 
     def __str__(self):
         return self.nombre
@@ -22,14 +33,8 @@ class Cliente(models.Model):
     # Atributos del modelo
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
-    # Otros campos y relaciones
+    codigo_cilindro = models.ForeignKey(Cilindro, on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         return self.nombre
-class Cilindro(models.Model):
-    # Atributos del modelo
-    codigo = models.CharField(max_length=50)
-    capacidad = models.IntegerField()
-    # Otros campos y relaciones
-
-    def __str__(self):
-        return self.codigo
+ 
--- project/apps/HOME/views.py
+++ project/apps/HOME/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Cilindro
 
 # Create your views here.
 def index (request):
     return render (request, "HOME/index.html", {"buscar": "busca por codigo"})
+
+def home (request):
+    registro_cilindros = Cilindro.objects.all()
+    
